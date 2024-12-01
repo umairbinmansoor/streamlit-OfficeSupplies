bab0cef9901d7f168d506f99972993bbcef4fd14
umairbinmansoor <42925409+umairbinmansoor@users.noreply.github.com>
2024-12-01 15:05:43 +0500

    Update stream_dash2.py

--- stream_dash2.py
+++ stream_dash2.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')  # Set the backend before importing pyplot
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
